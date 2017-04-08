c15358aa84359ea48dfe03e3b8d639cc7d731bcd
williamharefa <williamjharefa@gmail.com>
2017-04-08 11:39:51 +0700

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Fibonacci {
+    public static void main(String[]args) {
+        Scanner show = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Deret Fibonacci ");
+        int n = show.nextInt();
+        long fib[] = new long[n];
+         
+        fib[0] = 0;
+        fib[1] = 1;
+         
+        for(int i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+         
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+    }
+ 
+}
